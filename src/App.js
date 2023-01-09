a8398a23dcab172c54c46439bc4654200156c088
Joao Lino <linusmannuel@gmail.com>
2023-01-09 03:08:55 +0100

    feat: add theme provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from "react";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/tema";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaEscuro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/UI/tema.jsx
+++ src/Components/UI/tema.jsx
@@ -12,3 +12,9 @@ export const temaClaro = {
   inside: conteudoClaro,
   text: textoFundoClaro,
 }
+
+export const temaEscuro = {
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro,
+}
