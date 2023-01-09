1bf4679a09b55f7f3fde0ea613d53e0012658e8d
Joao Lino <linusmannuel@gmail.com>
2023-01-09 02:54:18 +0100

    feat: add variables colors

--- src/Components/UI/variaveis.jsx
+++ src/Components/UI/variaveis.jsx
@@ -1,3 +1,9 @@
 export const corPrimaria = "#41d3b2"
+
 export const fundoClaro = "#f1f1f1"
 export const textoFundoClaro = "grey"
+export const conteudoClaro = "#fff"
+
+export const fundoEscuro = "#363537"
+export const conteudoEscuro = "#5c5b5e"
+export const textoFundoEscuro = "#fAfAfA"
